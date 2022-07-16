1b93951f7132b4f28560ca6bf77cde85677f756e
Gabriel Murua <gmuruadev@gmail.com>
2022-07-16 20:41:00 -0300

    agregando inicio y fin de juego

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 const root = document.querySelector('.root');
-let oldDeck = ['A','A',2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,'J','J','Q','Q','K','K'];
 let deck = [];
 let valueCard1 = '';
 let valueCard2 = '';
 let card1;
 let card2;
+let pairs;
+const start = document.querySelector('.start');
+let timeStart = '';
+
+const newGame = () => {
+    console.log('starting new game');
+    start.classList.toggle('hidden');
+    root.innerHTML = '';
+    let oldDeck = ['A','A',2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,'J','J','Q','Q','K','K'];
+    deck = [];
+    valueCard1 = '';
+    valueCard2 = '';
+    pairs = 0;
+    timeStart = new Date().getTime();
+
+    // shuffling deck
+    do {
+        let index = Math.floor(Math.random() * (oldDeck.length-1));
+        deck.push(oldDeck[index]);
+        oldDeck.splice(index,1);
+    } while (oldDeck.length > 0)
+
+    // painting shuffled deck
+    for ( let i=0; i<deck.length ; i++ ) {
+        root.innerHTML += `<div id='id${(i+1)}' class='card'></div>`;
+    }
+
+    // adding listeners
+    for ( let j=0; j<deck.length ; j++ ) {
+        document.querySelector(`#id${(j+1)}`).addEventListener('click', seeCard);
+    }
+}
 
 const flipToShow = (card,value) => {
     card.classList.add('flipToShowStep1');
@@ -30,6 +61,17 @@ const flipToHide = (card) => {
     }, 800);
 }
 
+const end = () =>{
+    start.classList.toggle('hidden');
+    let timeEnd = new Date().getTime();
+    // round to 1 decimal place
+    let time = (Math.trunc((timeEnd - timeStart) /100) /10);
+    console.log('completed in', time, 's');
+    document.querySelector('.info').innerHTML = `
+        <div>Completed in ${time} seconds</div>
+    `;
+}
+
 const seeCard = (e) => {
     let id = e.target.id;
     if ((valueCard2 === '') && !(e.target.classList.contains('exposed'))) {
@@ -50,6 +92,12 @@ const seeCard = (e) => {
             console.log('found pair of',valueCard1);
             valueCard1 = '';
             valueCard2 = '';
+            pairs++;
+            if (pairs === 12) {
+                setTimeout(()=>{
+                    end();
+                }, 800);
+            }
             return;
         }
     
@@ -64,19 +112,4 @@ const seeCard = (e) => {
     }
 }
 
-// shuffling deck
-do {
-    let index = Math.floor(Math.random() * (oldDeck.length-1));
-    deck.push(oldDeck[index]);
-    oldDeck.splice(index,1);
-} while (oldDeck.length > 0)
-
-// painting shuffled deck
-for ( let i=0; i<deck.length ; i++ ) {
-    root.innerHTML += `<div id='id${(i+1)}' class='card'></div>`;
-}
-
-// adding listeners
-for ( let j=0; j<deck.length ; j++ ) {
-    document.querySelector(`#id${(j+1)}`).addEventListener('click', seeCard);
-}
+document.querySelector('.start > button').addEventListener('click',newGame);
