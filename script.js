dd61f0a3632c1fce6a867427b77dcedf22884610
Gabriel Murua <gmuruadev@gmail.com>
2022-07-16 15:42:40 -0300

    eliminando pasos innecesarios en seeCard()

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ let oldDeck = ['A','A',2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,'J','J','Q','Q','K','K'];
 let deck = [];
 let valueCard1 = '';
 let valueCard2 = '';
-let match = false;
 let card1;
 let card2;
 
 const seeCard = (e) => {
     let id = e.target.id;
-                               // clic on cards exposed do nothing
     if ((valueCard2 === '') && !(e.target.classList.contains('exposed'))) {
         if (valueCard1 === '') {
             valueCard1 = deck[id.slice(2)-1];
@@ -19,32 +17,26 @@ const seeCard = (e) => {
             return;
         }
     
-        if (valueCard2 === '') {
-            valueCard2 = deck[id.slice(2)-1];
-            card2 = document.getElementById(`${id}`);
-            card2.classList.add('exposed');
-            card2.innerText = valueCard2;
+        valueCard2 = deck[id.slice(2)-1];
+        card2 = document.getElementById(`${id}`);
+        card2.classList.add('exposed');
+        card2.innerText = valueCard2;
 
-            if (valueCard1 === valueCard2) match = true
-        }
-    
-        if ((valueCard2 != '') && (match == false)) {
-            setTimeout(()=>{
-                valueCard1 = '';
-                valueCard2 = '';
-                card1.innerText = '';
-                card1.classList.toggle('exposed');
-                card2.innerText = '';
-                card2.classList.toggle('exposed');
-            },1000);
-        }
-
-        if (match) {
+        if (valueCard1 === valueCard2) {
             console.log('found pair of',valueCard1);
             valueCard1 = '';
             valueCard2 = '';
-            match = false;
+            return;
         }
+    
+        setTimeout(()=>{
+            valueCard1 = '';
+            valueCard2 = '';
+            card1.innerText = '';
+            card1.classList.toggle('exposed');
+            card2.innerText = '';
+            card2.classList.toggle('exposed');
+        },1000);
     }
 }
 
